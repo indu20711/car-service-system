7fa7a24c098410ce889dc985f465f5bc470e7473
indupitta2003 <166369142+indu20711@users.noreply.github.com>
2025-04-04 19:01:29 +0530

    Update customerLogin.java

--- customerLogin.java
+++ customerLogin.java
@@ -5,37 +5,39 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import com.gqt.model.car;
+import com.gqt.model.Customer;
 
 
-public class UpdateStatus extends HttpServlet {
-	
+
+
+
+public class customerLogin extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String carRegistrationNumber = request.getParameter("carRegistrationNumber");
-		
-		String serviceStatus = request.getParameter("status");
-		
-		
-		car tempcar = new car();
-		tempcar.setCarRegistrationNumber(carRegistrationNumber);
-		tempcar.setServiceType(serviceStatus);
-
-		System.out.println("controller"+serviceStatus+" "+carRegistrationNumber);
-		int status = tempcar.updateService();
-
-		System.out.println("controller"+serviceStatus+" "+carRegistrationNumber);
-		if(status==1) {
-			response.sendRedirect("/car-service-system/updateStatusSuccess.jsp");
-			
-		}
-		else {
-			response.sendRedirect("/car-service-system/updateStatusFailed.jsp");
-		}
-		
+	String UserName = request.getParameter("UserName");
+	String Password = request.getParameter("Password");
+	
+	Customer tempCustomer = new Customer();
+	
+	tempCustomer.setUserName(UserName);
+	tempCustomer.setPassword(Password);
+	int status = tempCustomer.customerLogin();
+	String name = tempCustomer.getName();
+	System.out.println("Controller "+UserName+" "+Password);
+	
+	HttpSession session = request.getSession();
+	session.setAttribute("sname", name);//String sname = name 
+	session.setAttribute("susername", UserName);
+	if(status==1) {
+		response.sendRedirect("/car-service-system/customerLoginSuccess.jsp");
 	}
-
+	else if (status == 0){
+		response.sendRedirect("/car-service-system/customerInvalidPassword.jsp");
+	}
+	else {
+		response.sendRedirect("/car-service-system/customerInvalidUserName.jsp");
+	}
 }
-
+}
